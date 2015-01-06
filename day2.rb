3e0fdd9da0db6b27ab66c18eeedce067854ac8fb
Lindsay Williams <williamsl25@yahoo.com>
2015-01-06 12:05:32 -0500

    first commit

---
+++ day2.rb
@@ -0,0 +1,31 @@
+ [] - brackets
+{} curly brackets
+array1 = [1,2,3,"jessie",4, "string"]
+puts array1.inspect
+puts array1[0].inspect
+puts array1[4].inspect
+puts array1[3].inspect
+
+students = ["jessie", "adam", "Jackie","troy"]
+puts students[2].inspect
+students.push("robbi")
+
+
+students = [
+["jessie", 'adam']]
+
+
+puts students [0][0]
+
+dog_types = ["pugs", "schnouzer", "i hate you guys"]
+dog_types.push("beagles")
+puts dog_types.inspect
+puts dog_types.pop.inspect
+
+fish = [
+		["tuna", "mackeral"],
+		["scup", "catfish"]
+]
+puts fish[0].length
+puts fish[1].length
+# will print 
